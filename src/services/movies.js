e6b245c17b85b8a8162f0a60002ad41667605437
Joseph Kinuthia <cjkinuthia5@gmail.com>
2024-05-01 08:06:16 +0300

    fixed undefined error in home

--- src/services/movies.js
+++ src/services/movies.js
@@ -5,14 +5,17 @@ const API_KEY = import.meta.env.VITE_API_KEY
 
 export const getMovies = async(genre) => {
     const response = await axios.get(`${baseUrl}${genre}/movie/day?api_key=${API_KEY}&page=1`)
-    return response.data.results
+    const results = response.data.results ? response.data.results : []
+    return results
 }
 
 export const getTrending = async() => {
     const response = await axios.get(`${baseUrl}trending/all/day?api_key=${API_KEY}`)
-    return response.data.results
+    const results = response.data.results ? response.data.results : []
+    return results
 }
 export const getTop = async(type) => {
     const response = await axios.get(`${baseUrl}${type}/top_rated?api_key=${API_KEY}`)
-    return response.data.results
+    const results = response.data.results ? response.data.results : []
+    return results
 }
