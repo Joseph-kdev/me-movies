c9acc2f5d57a3c5d6533820d61bb3c4d8ac40f97
Joseph-kdev <cjkinuthia5@gmail.com>
2025-05-03 21:22:21 +0300

    Added new image to carousel

--- src/components/movieList.jsx
+++ src/components/movieList.jsx
@@ -1,30 +1,29 @@
-import { Movie } from "./Movie"
-import './stylesheets/movielist.css'
-
+import { Movie } from "./Movie";
+import "./stylesheets/movielist.css";
 
 export const MovieList = ({ movies }) => {
-
-    const movielist = movies ? movies : []
-
+  const movielist = movies ? movies : [];
   return (
     <>
-        <div id="movie-list">
-            <ul>
-                {movielist.map(movie => (
-                    <li key={movie.id}>
-                        <Movie
-                         id = {movie.id}
-                         title={movie.title ? movie.title : movie.name}
-                         overview={movie.overview}
-                         poster_path={movie.poster_path}
-                         vote_average={movie.vote_average}
-                         release_date={movie.release_date ? movie.release_date : movie.first_air_date}
-                         type = {movie.type || (movie.name ? 'tv' : 'movie')}
-                         />
-                    </li>
-                ))}
-            </ul>
-        </div>
+      <div id="movie-list">
+        <ul>
+          {movielist.map((movie) => (
+            <li key={movie.id}>
+              <Movie
+                id={movie.id}
+                title={movie.title ? movie.title : movie.name}
+                overview={movie.overview}
+                poster_path={movie.poster_path}
+                vote_average={movie.vote_average}
+                release_date={
+                  movie.release_date ? movie.release_date : movie.first_air_date
+                }
+                type={movie.type || (movie.name ? "tv" : "movie")}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
-}
+  );
+};
