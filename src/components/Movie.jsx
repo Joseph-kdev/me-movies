c5b7c74744dec5590bea50230d23077a8d894b47
Joseph-kdev <cjkinuthia5@gmail.com>
2025-07-25 16:11:45 +0300

    spiced up the movie cards

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { useUserAuth } from "../UserAuthContext";
 import {
@@ -212,10 +212,33 @@ export const Movie = ({
   const limitedOverview =
     overview.substring(0, 160) + (overview.length > 160 ? "..." : "");
 
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          ref.current.dataset.inView = "true";
+          
+        }
+      },
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
   return (
     <>
       <div className="movie">
-        <div className="movie-poster">
+        <div className="movie-poster"  ref={ref} data-in-view="false">
           <img
             src={`${imgUrl}${poster_path}`}
             alt={title}
@@ -226,56 +249,73 @@ export const Movie = ({
               <h3>{title}</h3>
             </div>
           </div>
-            <div className="heroinIcons favorite">
-              {movieInCollections.favorites ? (
-                <svg
-                  onClick={() => toggleMovieCollection("favorites", "remove")}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#a3dcbc"
-                  strokeWidth={1.5}
-                  stroke="#a3dcbc"
-                  className="w-6 h-6"
-                  width="24px"
-                  height="24px"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  onClick={() => toggleMovieCollection("favorites", "add")}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  strokeWidth={1.5}
-                  stroke="#a3dcbc"
-                  className="w-6 h-6"
-                  width="24px"
-                  height="24px"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                  />
-                </svg>
-              )}
-            </div>
+          <div className="heroinIcons favorite">
+            {movieInCollections.favorites ? (
+              <svg
+                onClick={() => toggleMovieCollection("favorites", "remove")}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#a3dcbc"
+                strokeWidth={1.5}
+                stroke="#a3dcbc"
+                className="w-6 h-6"
+                width="24px"
+                height="24px"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                />
+              </svg>
+            ) : (
+              <svg
+                onClick={() => toggleMovieCollection("favorites", "add")}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                strokeWidth={1.5}
+                stroke="#a3dcbc"
+                className="w-6 h-6"
+                width="24px"
+                height="24px"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                />
+              </svg>
+            )}
+          </div>
+          <div className="rating">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="gold"
+                className="w-5 h-5"
+                width="16px"
+                height="16px"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            <p>{vote_average}</p>
+          </div>
         </div>
       </div>
       <Modal
         isOpen={open}
         onRequestClose={closeModal}
         shouldCloseOnOverlayClick={true}
-        ariaHideApp = {false}
-        className= "modal"
-        style = {{
-          overlay : {
-            backgroundColor: '#000000de',
-            zIndex: '99'
-          }
+        ariaHideApp={false}
+        className="modal"
+        style={{
+          overlay: {
+            backgroundColor: "#000000de",
+            zIndex: "99",
+          },
         }}
       >
         <div className="movie-modal">
